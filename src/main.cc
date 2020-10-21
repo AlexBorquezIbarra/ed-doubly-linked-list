38b6486ab69697ddd5c243692f8ce83ef1cc93f6
Alex Borquez Ibarra <alex_borquez@live.com.mx>
2020-10-21 14:26:04 -0600

    Subida3Parcial

---
+++ src/main.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include "List.hh"
+
+int main()
+{
+    List* list{new List()};
+
+    //std::cout << "hello list" << std::endl;
+
+    list->PrintBack();
+    list->PrintFront();
+
+    std::cin.get();
+
+    return 0;
+}
+#include<iostream>
+#include "List.hh"
+
+int main()
+{
+    List* list{new List()};
+
+    //std::cout << "hello list" << std::endl;
+
+    list->PrintBack();
+    list->PrintFront();
+
+    std::cin.get();
+
+    return 0;
+}
